47e71081eae0e7ec88e16b1224b0e6864c0a132e
kindichi <congvinhdc89@gmail.com>
2021-01-27 19:51:19 +0700

    second

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class NewsController extends Controller
+class BrandController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class NewsController extends Controller
      */
     public function index()
     {
-        echo 'index';
+        //
     }
 
     /**
@@ -23,7 +23,7 @@ class NewsController extends Controller
      */
     public function create()
     {
-        echo 'Create';
+        //
     }
 
     /**
